19d477aff4593ab8a81005ac5bc3b06c334ab260
Khongchai Greesuradej <56601823+Khongchai@users.noreply.github.com>
2021-10-27 00:42:46 +0700

    Update delayedMouse.ts

--- delayedMouse.ts
+++ delayedMouse.ts
@@ -1,10 +1,5 @@
-/**
- * Call this whereever requestanimationframe() is called.
- *
- * To use the delayed coordinate (tween, ease, whatever you wanna call it),
- * call the updateMouse method in the request animationframe() and use the returned x, y coordinate
- */
-export class DelayedMouse {
+
+export class Lerp{
   curX: number;
   curY: number;
   delayedX: number;
@@ -22,11 +17,10 @@ export class DelayedMouse {
     this.speedDiff = speedDiff;
   }
 
-  updateMouse(curX: number, curY: number, delta: number) {
+  updateCoords(curX: number, curY: number, delta: number) {
     this.curX = curX;
     this.curY = curY;
-
-    //Calculate delayed mouse
+    
     this.calculateDelayed();
 
     return { x: this.delayedX, y: this.delayedY };
